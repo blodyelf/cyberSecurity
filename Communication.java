dc75954f5300258edfd314159325895ac2082cda
blodyelf <31694590+blodyelf@users.noreply.github.com>
2018-02-27 18:54:10 +0000

    a

--- Communication.java
+++ Communication.java
@@ -1,3 +1,42 @@
-public class Communication{
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.SocketFactory;
+import javax.net.ssl.HttpsURLConnection;
 
+public class Communication{
+    
+    public Communication(){
+        
+    }
+    
+    public static void main(String args[]){
+        
+        
+    }
+    
+    
+    
+    private SSLSocketFactory createSslSocketFactory() throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException, KeyManagementException {
+        // load up the key store
+        String STORETYPE = "JKS";
+        String KEYSTORE = "keystore.jks";
+        String STOREPASSWORD = "storepassword";
+        String KEYPASSWORD = "keypassword";
+
+        KeyStore ks = KeyStore.getInstance(STORETYPE);
+        ks.load(MattermostClient.class.getResourceAsStream(KEYSTORE), STOREPASSWORD.toCharArray());
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+        kmf.init(ks, KEYPASSWORD.toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(ks);
+
+        SSLContext sslContext = null;
+        sslContext = SSLContext.getInstance("TLS");
+        //		sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+        sslContext.init(null, null, null); // will use java's default key and trust store which is sufficient unless you deal with self-signed certificates
+
+        return sslContext.getSocketFactory();// (SSLSocketFactory) SSLSocketFactory.getDefault();
+    }
+ 
 }
---
+++ compile.bat
@@ -0,0 +1,5 @@
+@echo off
+
+javac Communication.java 
+
+pause
